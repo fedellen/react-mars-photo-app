3add0ade70df5b27273636f29a684a2206e82ee2
fedellen <fedellen@pixelpajamastudios.com>
2021-05-13 11:19:35 -0500

    Adjust `App` test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Title loads', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Latest photos from Mars 🚀/i);
   expect(linkElement).toBeInTheDocument();
 });
