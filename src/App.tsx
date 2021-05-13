868867aca9db0d754a4eb3d00d33ac2d83d8a790
fedellen <fedellen@pixelpajamastudios.com>
2021-05-12 19:11:46 -0500

    Initially render mars photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const marsApi = `https://api.nasa.gov/mars-photos/api/v1/rovers/Perseverance/latest_photos?api_key=DEMO_KEY`;
+
+type marsObject = {
+  latest_photos: [
+    {
+      id: number;
+      sol: number;
+      img_src: string;
+      earth_date: string;
+      rover: {
+        id: number;
+        landing_date: string;
+        launch_date: string;
+        name: string;
+        status: string;
+      };
+    }
+  ];
+};
+
 function App() {
+  const [marsData, setMarsData] = useState<marsObject | null>(null);
+
+  async function fetchData() {
+    try {
+      const response: marsObject = await (await fetch(marsApi)).json();
+      setMarsData(response);
+    } catch (err) {
+      console.error('Failed to fetch Mars API ', err);
+    }
+  }
+
+  console.log(marsData);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // const trimmedData = marsData?.photos.slice(0, 20);
+
+  // console.log(trimmedData);
+
+  const displayUrls: string[] = marsData
+    ? marsData.latest_photos.slice(0, 20).map((data) => data.img_src)
+    : [];
+
+  let key = 0;
+  const keyGen = () => {
+    key++;
+    return key;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Latest photos from Mars</h1>
+      {displayUrls.map((url) => (
+        <img className='photo' src={url} key={keyGen()} alt='Mars photos' />
+      ))}
     </div>
   );
 }
