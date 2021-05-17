26eea0336a929c467269b7fc505aef7915577cde
fedellen <fedellen@pixelpajamastudios.com>
2021-05-17 13:26:59 -0500

    Adjust gallery styling

--- src/components/DisplayPhotoGroup.tsx
+++ src/components/DisplayPhotoGroup.tsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import { useStateValue } from "../state/state";
 import { marsPhoto } from "../types";
 import { keyGen } from "../utils/keyGen";
+
+type PhotoProps = {
+  photo: marsPhoto;
+  currentlySelected: boolean;
+  selectPhoto: () => void;
+};
+
+function Photo({ photo, currentlySelected, selectPhoto }: PhotoProps) {
+  const [zoom, setZoom] = useState(false);
+
+  return (
+    <div className={currentlySelected ? `active ${zoom ? "zoom" : ""}` : ""}>
+      <img
+        className={`photo`}
+        src={photo.img_src}
+        key={keyGen()}
+        alt="Photograph from a Mars rover"
+        loading="lazy"
+        onClick={() => (currentlySelected ? setZoom(!zoom) : selectPhoto())}
+      />
+      {currentlySelected && (
+        <ul>
+          <li>Rover: {photo.rover.name}</li>
+          <li>Mars Sol: {photo.sol}</li>
+          <li>Earth Date: {photo.earth_date}</li>
+          <li>
+            <a href={photo.img_src}>Link to Raw Image</a>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+}
 
 type DisplayPhotoGroupProps = {
   photoGroup: marsPhoto[];
@@ -8,16 +43,37 @@ type DisplayPhotoGroupProps = {
 export default function DisplayPhotoGroup({
   photoGroup,
 }: DisplayPhotoGroupProps) {
+  const [{ selectedPhoto }, dispatch] = useStateValue();
   return (
     <section className="photoGroup">
       {" "}
       {photoGroup.map((photo) => (
-        <img
-          className="photo"
-          src={photo.img_src}
+        <Photo
           key={keyGen()}
-          alt="Photograph from a Mars rover"
+          photo={photo}
+          currentlySelected={selectedPhoto?.id === photo.id}
+          selectPhoto={() => dispatch({ type: "selectPhoto", payload: photo })}
         />
+        // <div>
+        //   <img
+        //     className="photo"
+        //     src={photo.img_src}
+        //     key={keyGen()}
+        //     alt="Photograph from a Mars rover"
+        //     loading="lazy"
+        //     onClick={() => dispatch({ type: "selectPhoto", payload: photo })}
+        //   />
+        //   {selectedPhoto?.id === photo.id && (
+        //     <ul>
+        //       <li>Rover: {photo.rover.name}</li>
+        //       <li>Mars Sol-{photo.sol}</li>
+        //       <li>Earth Date: {photo.earth_date}</li>
+        //       <li>
+        //         <a href={photo.img_src}>Link to Raw Image</a>
+        //       </li>
+        //     </ul>
+        //   )}
+        // </div>
       ))}
     </section>
   );
