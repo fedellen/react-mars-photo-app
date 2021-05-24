927b945178eb8210de98a44d224ce0a5a80d68f2
fedellen <fedellen@pixelpajamastudios.com>
2021-05-23 21:36:44 -0500

    Enable tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
@@ -16,6 +16,10 @@ module.exports = {
     },
     extend: {
       colors: {
+        dark: "#1d0b06",
+        bg: "#3a1004",
+        medium: "#581d0b",
+        text: "#db6744",
         ppjsLight: "#e6bb8f",
         ppjsOrange: "#f57f26",
         ppjsBlue: "#27A0F2",
