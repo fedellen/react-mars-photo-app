73075a4f45daca862e06bd54cdd557ac53382d0c
fedellen <fedellen@pixelpajamastudios.com>
2021-05-23 23:12:48 -0500

    Refactor / Restyle `DisplayPhotoGroup`

--- src/components/DisplayPhotoGroup.tsx
+++ src/components/DisplayPhotoGroup.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { useStateValue } from "../state/state";
 import { marsPhoto } from "../types";
 import { keyGen } from "../utils/keyGen";
@@ -13,9 +13,9 @@ function Photo({ photo, currentlySelected, selectPhoto }: PhotoProps) {
   const [zoom, setZoom] = useState(false);
 
   return (
-    <div className={currentlySelected ? `active ${zoom ? "zoom" : ""}` : ""}>
+    <div>
       <img
-        className={`photo`}
+        className="rounded-xl"
         src={photo.img_src}
         key={keyGen()}
         alt="Photograph from a Mars rover"
@@ -23,15 +23,31 @@ function Photo({ photo, currentlySelected, selectPhoto }: PhotoProps) {
         onClick={() => (currentlySelected ? setZoom(!zoom) : selectPhoto())}
       />
       {currentlySelected && (
-        <ul>
-          <li>Rover: {photo.rover.name}</li>
-          <li>Mars Sol: {photo.sol}</li>
-          <li>Earth Date: {photo.earth_date}</li>
-          <li>
-            <a href={photo.img_src}>Link to Raw Image</a>
-          </li>
-        </ul>
+        <div className="mt-3 text-xl translate-y-[-4.2rem] transform flex justify-between">
+          <PhotoInfo className="rounded-l-none">
+            <span>From the {photo.rover.name} Rover</span>
+            <a className="underline hover:text-red-600" href={photo.img_src}>
+              Link to Raw Image
+            </a>
+          </PhotoInfo>
+          <PhotoInfo className="rounded-r-none">
+            <span>Mars Sol (Days on Mars): {photo.sol}</span>
+            <span>Earth Date: {photo.earth_date}</span>
+          </PhotoInfo>
+        </div>
       )}
+    </div>
+  );
+}
+
+type PhotoInfoProps = { children?: ReactNode; className?: string };
+
+function PhotoInfo({ children, className }: PhotoInfoProps) {
+  return (
+    <div
+      className={`flex flex-col bg-bg bg-opacity-70 px-4 rounded-xl rounded-b-none ${className}`}
+    >
+      {children}
     </div>
   );
 }
@@ -45,35 +61,19 @@ export default function DisplayPhotoGroup({
 }: DisplayPhotoGroupProps) {
   const [{ selectedPhoto }, dispatch] = useStateValue();
   return (
-    <section className="photoGroup">
+    <section className="flex flex-wrap">
       {" "}
       {photoGroup.map((photo) => (
         <Photo
           key={keyGen()}
           photo={photo}
           currentlySelected={selectedPhoto?.id === photo.id}
-          selectPhoto={() => dispatch({ type: "selectPhoto", payload: photo })}
+          selectPhoto={() =>
+            selectedPhoto?.id === photo.id
+              ? dispatch({ type: "clearPhoto" })
+              : dispatch({ type: "selectPhoto", payload: photo })
+          }
         />
-        // <div>
-        //   <img
-        //     className="photo"
-        //     src={photo.img_src}
-        //     key={keyGen()}
-        //     alt="Photograph from a Mars rover"
-        //     loading="lazy"
-        //     onClick={() => dispatch({ type: "selectPhoto", payload: photo })}
-        //   />
-        //   {selectedPhoto?.id === photo.id && (
-        //     <ul>
-        //       <li>Rover: {photo.rover.name}</li>
-        //       <li>Mars Sol-{photo.sol}</li>
-        //       <li>Earth Date: {photo.earth_date}</li>
-        //       <li>
-        //         <a href={photo.img_src}>Link to Raw Image</a>
-        //       </li>
-        //     </ul>
-        //   )}
-        // </div>
       ))}
     </section>
   );
