2785e9161388eaf241938a4d3646f57da8bfe8e9
fedellen <fedellen@pixelpajamastudios.com>
2021-05-24 20:53:48 -0500

    Add functionality to hide info boxes on click

--- src/components/DisplayPhotoGroup.tsx
+++ src/components/DisplayPhotoGroup.tsx
@@ -48,9 +48,12 @@ function Photo({ photo, currentlySelected, selectPhoto }: PhotoProps) {
 type PhotoInfoProps = { children?: ReactNode; className?: string };
 
 function PhotoInfo({ children, className }: PhotoInfoProps) {
+  const [hide, setHide] = useState(false);
+  if (hide) return null;
   return (
     <div
       className={`flex flex-col bg-bg bg-opacity-80 px-4 rounded-xl rounded-b-none ${className}`}
+      onClick={() => setHide(true)}
     >
       {children}
     </div>
