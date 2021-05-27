bde2e1a98028f3cfc065a83d3e70b31de9e4ba8e
fedellen <fedellen@pixelpajamastudios.com>
2021-05-26 22:50:30 -0500

    Refactor `useMarsQuery` to use react-query pattern

--- src/hooks/useMarsQuery.ts
+++ src/hooks/useMarsQuery.ts
@@ -1,40 +1,32 @@
-import { useEffect } from "react";
+import { useQuery } from "react-query";
 import { useStateValue } from "../state/state";
-import { latestObject, solObject } from "../types";
+import { isLatest, latestObject, solObject } from "../types";
+import fetchFrom from "../utils/fetchFrom";
+
+const baseMarsApi = "https://mars-photos.herokuapp.com/api/v1/";
 
 export default function useMarsQuery() {
   const [{ apiQuery }, dispatch] = useStateValue();
-
-  const baseMarsApi = "https://mars-photos.herokuapp.com/api/v1/";
   const { rover, sol } = apiQuery;
+  const latestPhotos = sol === -1;
 
-  /** Get all manifest data on mount */
-  // useEffect(() => {}, [])
+  const currentQuery = `
+  ${baseMarsApi}/rovers/${rover}/${latestPhotos ? "latest_photos" : "photos"}${
+    latestPhotos ? "" : `?sol=${sol}`
+  }
+`;
 
-  /** Get new marsPhoto data when rover or sol change */
-  useEffect(() => {
-    async function fetchData() {
-      console.log("Sending query");
-      try {
-        const currentQuery = `
-          ${baseMarsApi}/rovers/${rover}/${
-          sol === -1 ? "latest_photos" : "photos"
-        }${sol === -1 ? "" : `?sol=${sol}`}
-        `;
-
-        if (sol === -1) {
-          const response: latestObject = await (
-            await fetch(currentQuery)
-          ).json();
-          dispatch({ type: "setData", payload: response.latest_photos });
-        } else {
-          const response: solObject = await (await fetch(currentQuery)).json();
-          dispatch({ type: "setData", payload: response.photos });
-        }
-      } catch (err) {
-        console.error("Failed to fetch Mars API ", err);
-      }
+  function setData(data: latestObject | solObject) {
+    if (isLatest(data)) {
+      dispatch({ type: "setData", payload: data.latest_photos });
+    } else {
+      dispatch({ type: "setData", payload: data.photos });
     }
-    fetchData();
-  }, [rover, sol, dispatch]);
+  }
+  const { error } = useQuery<latestObject | solObject>(
+    `marsPhotos-${rover}-sol`,
+    () => fetchFrom(currentQuery),
+    { onSuccess: (data) => setData(data) }
+  );
+  if (error) console.error("Error on photo query", error);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,16 @@ import "./style/index.css";
 import App from "./App";
 import StateProvider from "./state/state";
 import { reducer } from "./state/reducer";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <StateProvider reducer={reducer}>
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </React.StrictMode>
   </StateProvider>,
   document.getElementById("root")
