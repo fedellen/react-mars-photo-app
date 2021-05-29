d677100cf5346e70a965c7d003d015d826aa847f
fedellen <fedellen@pixelpajamastudios.com>
2021-05-29 17:19:27 -0500

    Reset `currentCamera` on query change

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -32,6 +32,7 @@ export const reducer = (state: State, action: Action): State => {
     case "changeQuery":
       return {
         ...state,
+        currentCamera: "all",
         apiQuery: action.payload,
       };
 
