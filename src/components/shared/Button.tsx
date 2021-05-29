d8af19f0b566faaaa86f367bce64e132b9d534bd
fedellen <fedellen@pixelpajamastudios.com>
2021-05-28 22:25:44 -0500

    Add classNames to shared Button

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -8,12 +8,19 @@ type ButtonProps = DetailedHTMLProps<
   onClick: () => void;
   /** Text or component on button */
   children?: ReactNode;
+  /** Additional TailwindCSS styles or class names for other uses */
+  className?: string;
 };
 
-export default function Button({ onClick, children, ...props }: ButtonProps) {
+export default function Button({
+  onClick,
+  children,
+  className,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      className="p-2 lg:px-4 text-xs lg:text-base  rounded-full shadow-md bg-medium text-bg font-bold hover:bg-light"
+      className={`p-2 lg:px-4 text-xs lg:text-base  rounded-full shadow-md bg-medium text-bg font-bold hover:bg-light ${className}`}
       onClick={onClick}
       {...props}
     >
